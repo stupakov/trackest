81490f81354f4146842076d07dc049c794afed83
Andrew Bruce and Ruben Infante <pair+andrew+ruben@pivotallabs.com>
2013-05-23 18:36:52 -0700

    WIP more progress

--- trackest.js
+++ trackest.js
@@ -1,15 +1,33 @@
 $(function() {
-    tracker.app.renderBuffer().afterFlush().done(function() {
-        $(".add_story").before("<p class='add_templated_story'>Add Templated Story</p>");
-    });
+  tracker.app.renderBuffer().afterFlush().done(function() {
+      $(".add_story").before("<p class='add_templated_story'>Add Templated Story</p>");
+  });
 });
 
-$(".add_templated_story").click(function() {
-    tracker.app.renderBuffer().afterFlush().done(function() {
-        var editor = $(".story.new .editor.description");
-        if (editor.val() !== "") return;
+function populateNewEditor() {
+  console.log("populating");
+  var editor = $(".story.new .editor.description");
+  if (editor.val() !== "") return;
 
-        editor.val("Who does this benefit?\n\nWhat needs to change?\n\nWhat are the acceptance criteria?");
-    });
-    $(".add_story").click();
+  $(".story.new label.description .resizer.markup").css("visibility", "hidden");
+  editor.val("Who does this benefit?\n\nWhat needs to change?\n\nWhat are the acceptance criteria?");
+
+  //TODO: trigger resize
+}
+
+$(".add_templated_story").click(function() {
+  alert("OKAY");
+  var initialNewStoryCount = $(".story.new").length;
+  var checkingInterval;
+  function checkIfNewStoryAdded() {
+    console.log("checking");
+    if ($(".story.new").length >= initialNewStoryCount) {
+      window.clearInterval(checkingInterval);
+      populateNewEditor();
+    }
+  }
+  checkingInterval = window.setInterval(checkIfNewStoryAdded, 1000);
+
+  //tracker.app.renderBuffer().afterFlush().done(populateNewEditor);
+  $(".add_story").click();
 });
